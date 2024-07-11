52d05861791c382c9c257a06a01c27d9f92cabff
Sunil Kumar Malani <sunilmalani456@gmail.com>
2024-07-12 00:07:18 +0530

    backend complete.

--- Backend/src/db/index.js
+++ Backend/src/db/index.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
+  console.log(`MongoDB Connecting ...`);
   try {
     const connectionInstance = await mongoose.connect(
       `${process.env.MONGODB_URL}/${DB_NAME}`
